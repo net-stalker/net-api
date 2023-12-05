c6fabac737ab7108c3781b9a9312c299d8391d3e
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-12-05 13:02:51 +0200

    feature/CU-8693926wv: almost finished implementing filters api

--- net-timescale-api/src/api/mod.rs
+++ net-timescale-api/src/api/mod.rs
@@ -5,6 +5,8 @@ pub mod dashboard;
 //TODO: Add network prefix
 pub mod bandwidth_per_endpoint;
 
+pub mod overview_dashboard_filters;
+
 pub mod network_bandwidth;
 pub mod network_graph;
 pub mod network_packet;
