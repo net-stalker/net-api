0ef421474a3cc66b968ac5f8dd2d7f1351eb6846
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-02-22 10:30:49 +0200

    NS-112/add-filters-to-bpp: implemented filters for network bandwidth per protocol chart api, added the strcuture to request dto structure

--- net-reporter-api/src/api/network_bandwidth_per_protocol/mod.rs
+++ net-reporter-api/src/api/network_bandwidth_per_protocol/mod.rs
@@ -1,4 +1,5 @@
 #[allow(clippy::module_inception)]
 pub mod network_bandwidth_per_protocol;
 pub mod network_bandwidth_per_protocol_request;
+pub mod network_bandwidth_per_protocol_filters;
 pub mod protocol;
