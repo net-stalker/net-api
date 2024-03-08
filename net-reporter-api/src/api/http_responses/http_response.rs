2c583012223b7f0e36a0d0fca740a00cd8c84a17
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-03-08 17:18:29 +0200

    NS-179/dto-for-http-responses: updated api due to some changes in query

--- net-reporter-api/src/api/http_responses/http_response.rs
+++ net-reporter-api/src/api/http_responses/http_response.rs
@@ -6,6 +6,7 @@ use ion_rs::IonWriter;
 
 use ion_rs::ReaderBuilder;
 
+use ion_rs::StreamItem;
 use net_core_api::api::API;
 use net_core_api::encoder_api::Encoder;
 use net_core_api::decoder_api::Decoder;
@@ -15,7 +16,7 @@ const DATA_TYPE: &str = "http_client";
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct HttpResponseDTO {
-    bucket_timestamp: i64,
+    date: Option<i64>,
     client: String,
     server: String,
     response: i64,
@@ -23,17 +24,17 @@ pub struct HttpResponseDTO {
 impl API for HttpResponseDTO { }
 
 impl HttpResponseDTO {
-    pub fn new(bucket_timestamp: i64, client: &str, server: &str, response: i64) -> Self {
+    pub fn new(date: Option<i64>, client: &str, server: &str, response: i64) -> Self {
         HttpResponseDTO {
-            bucket_timestamp,
+            date,
             client: client.to_string(),
             server: server.to_string(),
             response,
         }
     }
 
-    pub fn get_bucket_timestamp(&self) -> i64 {
-        self.bucket_timestamp
+    pub fn get_date(&self) -> Option<i64> {
+        self.date
     }
 
     pub fn get_client(&self) -> &str {
@@ -58,8 +59,11 @@ impl Encoder for HttpResponseDTO {
         
         writer.step_in(IonType::Struct).expect("Error while creating an ion struct");
         
-        writer.set_field_name("bucket_timestamp");
-        writer.write_i64(self.bucket_timestamp).unwrap();
+        writer.set_field_name("date");
+        match self.date {
+            Some(date) => writer.write_i64(date).unwrap(),
+            None => writer.write_null(IonType::Int).unwrap(),
+        };
 
         writer.set_field_name("client");
         writer.write_string(&self.client).unwrap();
@@ -82,10 +86,16 @@ impl Decoder for HttpResponseDTO {
         let mut binary_user_reader = ReaderBuilder::new().build(data).unwrap();
         binary_user_reader.next().unwrap();
         binary_user_reader.step_in().unwrap();
-
-        binary_user_reader.next().unwrap();
-        let bucket_timestamp = binary_user_reader.read_i64().unwrap();
         
+        binary_user_reader.next().unwrap();
+        let date = match binary_user_reader.current() {
+            StreamItem::Value(_) => {
+                let date = binary_user_reader.read_i64().unwrap();
+                Some(date)
+            },
+            _ => None,
+        };
+
         binary_user_reader.next().unwrap();
         let binding = binary_user_reader.read_string().unwrap();
         let client = binding.text();
@@ -99,7 +109,7 @@ impl Decoder for HttpResponseDTO {
 
         binary_user_reader.step_out().unwrap();
 
-        HttpResponseDTO::new(bucket_timestamp, client, server, response)
+        HttpResponseDTO::new(date, client, server, response)
     }
 }
 
@@ -131,14 +141,14 @@ mod tests {
         const CLIENT: &str = "0.0.0.0";
         const SERVER: &str = "1.1.1.1";
         const RESPONSE: i64 = 200;
-        let http_response = HttpResponseDTO::new(BUCKET_TIMESTAMP, CLIENT, SERVER, RESPONSE);
+        let http_response = HttpResponseDTO::new(Some(BUCKET_TIMESTAMP), CLIENT, SERVER, RESPONSE);
         let mut binary_user_reader = ReaderBuilder::new().build(http_response.encode()).unwrap();
 
         assert_eq!(StreamItem::Value(IonType::Struct), binary_user_reader.next().unwrap());
         binary_user_reader.step_in().unwrap();
 
         assert_eq!(StreamItem::Value(IonType::Int), binary_user_reader.next().unwrap());
-        assert_eq!("bucket_timestamp", binary_user_reader.field_name().unwrap());
+        assert_eq!("date", binary_user_reader.field_name().unwrap());
         assert_eq!(BUCKET_TIMESTAMP, binary_user_reader.read_i64().unwrap());
         
         assert_eq!(StreamItem::Value(IonType::String), binary_user_reader.next().unwrap());
@@ -158,11 +168,10 @@ mod tests {
 
     #[test]
     fn endec_http_response() {
-        const BUCKET_TIMESTAMP: i64 = 123456789;
         const CLIENT: &str = "0.0.0.0";
         const SERVER: &str = "1.1.1.1";
         const RESPONSE: i64 = 200;
-        let http_response = HttpResponseDTO::new(BUCKET_TIMESTAMP, CLIENT, SERVER, RESPONSE);
+        let http_response = HttpResponseDTO::new(None, CLIENT, SERVER, RESPONSE);
         assert_eq!(http_response, HttpResponseDTO::decode(&http_response.encode()));
     }
 }
