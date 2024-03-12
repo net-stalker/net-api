334c642deed7f1d61ddf65845ba0b8815b80c06c
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-03-12 14:03:39 +0200

    NS-171/dto-for-http-responses-dist-chart: fixed clippy error

--- net-reporter-api/src/api/http_responses_distribution/mod.rs
+++ net-reporter-api/src/api/http_responses_distribution/mod.rs
@@ -1,4 +1,5 @@
 pub mod http_responses_disribution_filters;
 pub mod http_responses_distribution_bucket;
 pub mod http_responses_distribution_request;
+#[allow(clippy::module_inception)]
 pub mod http_responses_distribution;
